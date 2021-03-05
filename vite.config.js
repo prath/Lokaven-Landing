45c9ae2639f4f6c647ff8250843ad9847aee1257
Pratama Hasriyan <pratama@hasriyan.com>
2021-03-05 10:43:50 +0700

    leftovers

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-// import { getAliases } from 'vite-aliases'
 
-// const aliases = getAliases();
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
